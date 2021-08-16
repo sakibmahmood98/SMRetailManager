b1565aa43f689929598a3fb039d7940ca64dfd78
sakibmahmood98 <sakibmahmood09@gmail.com>
2021-08-17 00:11:02 +0600

    Register DB Tables
    The tables for the basic register system.

---
+++ SMData/dbo/Tables/Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductName] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
