af1d8bb570aea87445a2e749bf3876b1d8d9e356
sakibmahmood09 <sakibmahmood09@gmail.com>
2021-08-18 20:43:59 +0600

    Wire Up Login Form to API

--- SMRDesktopUI/Bootstrapper.cs
+++ SMRDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using SMRDesktopUI.Helpers;
 using SMRDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SMRDesktopUI
 {
@@ -15,14 +17,20 @@ namespace SMRDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
         }
         protected override void Configure()
         {
             _container.Instance(_container);
-
+            
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                  .Singleton<IAPIHelper, APIHelper>();
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
